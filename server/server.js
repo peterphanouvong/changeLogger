eeb5272b9e1d66ae753f63aa2641e46aab06417d
peterphanouvong <z5164318@student.unsw.edu.au>
2020-02-25 22:48:38 +1100

    initial

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}!`);
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, build, 'index.html'));
+  });
+}
+
+
